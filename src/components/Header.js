792e91c292d358708b727568d01cdffc81176b20
Budhi Parkash <55031362+BudhiParkash@users.noreply.github.com>
2024-07-21 06:10:05 -0700

    style change

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,11 +12,11 @@ const Hamburger = () => (
   </svg>
 );
 
-const Logo = () => (
-  <svg id="logo-52" width="170" height="41" viewBox="0 0 170 41" fill="none" xmlns="http://www.w3.org/2000/svg">
-    {/* SVG path data here */}
-  </svg>
-);
+// const Logo = () => (
+//   <svg id="logo-52" width="170" height="41" viewBox="0 0 170 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+//     {/* SVG path data here */}
+//   </svg>
+// );
 
 const Header = () => {
   const [showNavbar, setShowNavbar] = useState(false);
